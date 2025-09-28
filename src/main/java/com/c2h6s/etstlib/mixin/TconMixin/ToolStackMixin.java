8c3970a6cff45fae57d57d584c674c8d02d73b20
EtSH-C2H6S <2130679254@qq.com>
2025-09-28 14:23:49 +0800

    堆叠工具不再能获得UUID，修复魔力修复实际上不消耗魔力的bug

--- src/main/java/com/c2h6s/etstlib/mixin/TconMixin/ToolStackMixin.java
+++ src/main/java/com/c2h6s/etstlib/mixin/TconMixin/ToolStackMixin.java
@@ -6,6 +6,7 @@ import com.c2h6s.etstlib.util.IToolUuidGetter;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
@@ -34,7 +35,7 @@ public abstract class ToolStackMixin implements IToolUuidGetter {
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void addUuidToNbt(Item item, ToolDefinition definition, CompoundTag nbt, CallbackInfo ci){
-        if (item instanceof IModifiable){
+        if (item instanceof IModifiable&&item.getMaxStackSize(new ItemStack(item))<=1){
             if (!nbt.contains(CommonConstants.KEY_TOOL_UUID,Tag.TAG_STRING)){
                 nbt.putString(CommonConstants.KEY_TOOL_UUID,UUID.randomUUID().toString());
             }
--- src/main/java/com/c2h6s/etstlib/tool/modifiers/Integration/BOTIntegration/ManaRepair.java
+++ src/main/java/com/c2h6s/etstlib/tool/modifiers/Integration/BOTIntegration/ManaRepair.java
@@ -14,7 +14,7 @@ public class ManaRepair extends EtSTBaseModifier {
     @Override
     public void modifierOnInventoryTick(IToolStackView tool, ModifierEntry modifier, Level world, LivingEntity holder, int itemSlot, boolean isSelected, boolean isCorrectSlot, ItemStack stack) {
         if (tool.getDamage()>0&&world.getGameTime()%5<=modifier.getLevel()&& holder instanceof Player player&&!world.isClientSide){
-            if (ManaItemHandler.INSTANCE.requestManaExactForTool(stack,player,200,false)){
+            if (ManaItemHandler.INSTANCE.requestManaExactForTool(stack,player,200,true)){
                 tool.setDamage(tool.getDamage()-1);
                 if (EtSTLib.random.nextInt(5)<modifier.getLevel()&&tool.getDamage()>0){
                     tool.setDamage(tool.getDamage()-1);
