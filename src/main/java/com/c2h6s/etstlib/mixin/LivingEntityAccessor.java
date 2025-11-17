5c8388d4e05526515f964fab489a072d5d134a2f
EtSH-C2H6S <2130679254@qq.com>
2025-11-17 20:55:18 +0800

    accessor和invoker的小修改

--- src/main/java/com/c2h6s/etstlib/mixin/LivingEntityAccessor.java
+++ src/main/java/com/c2h6s/etstlib/mixin/LivingEntityAccessor.java
@@ -8,5 +8,5 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(LivingEntity.class)
 public interface LivingEntityAccessor {
     @Invoker("getDamageAfterArmorAbsorb")
-    float getDamageAfterArmorAbsorb(DamageSource pDamageSource, float pDamageAmount);
+    float etstlib$getDamageAfterArmorAbsorb(DamageSource pDamageSource, float pDamageAmount);
 }
